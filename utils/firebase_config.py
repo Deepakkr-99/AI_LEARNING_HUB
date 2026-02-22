21d2b437e696630a5bbae0777a07b796b3404c84
Deepakkr-99 <dk719111@gmail.com>
2026-02-22 09:09:41 +0530

    Update firebase_config.py

--- utils/firebase_config.py
+++ utils/firebase_config.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
+import json
 
-firebase_dict = st.secrets["firebase"]
+# -------------------------
+# Get Firebase config from secrets
+# -------------------------
+firebase_json_str = st.secrets["firebase"]  # string from secrets
 database_url = st.secrets["FIREBASE_DATABASE_URL"]
 
+# Convert string to dict
+firebase_dict = json.loads(firebase_json_str)
+
+# Initialize Firebase once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(firebase_dict)
+    cred = credentials.Certificate(firebase_dict)  # now it's a dict
     firebase_admin.initialize_app(cred, {"databaseURL": database_url})
 
+# Database reference
 database = db.reference()
