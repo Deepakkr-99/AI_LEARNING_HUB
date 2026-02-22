a2d6d70e2d789271bbb0024bc68cfb18c6566714
Deepakkr-99 <dk719111@gmail.com>
2026-02-22 10:39:09 +0530

    Update 2_LearningHub.py

--- pages/2_LearningHub.py
+++ pages/2_LearningHub.py
@@ -5,9 +5,10 @@ genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 def ask_ai(question):
     try:
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-2.5-flash")
         response = model.generate_content(question)
         return response.text
     except Exception as e:
         import traceback
         return traceback.format_exc()
+
