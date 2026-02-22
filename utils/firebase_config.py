d41545f5ed52bc57701fe403cc99268f71422314
Deepakkr-99 <dk719111@gmail.com>
2026-02-22 08:16:26 +0530

    Update firebase_config.py

--- utils/firebase_config.py
+++ utils/firebase_config.py
@@ -2,15 +2,11 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 
-# Get Firebase config from secrets
 firebase_dict = st.secrets["firebase"]
 database_url = st.secrets["FIREBASE_DATABASE_URL"]
-gemini_key = st.secrets["GEMINI_API_KEY"]
 
-# Initialize Firebase once
 if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_dict)
     firebase_admin.initialize_app(cred, {"databaseURL": database_url})
 
-# Database reference
 database = db.reference()
